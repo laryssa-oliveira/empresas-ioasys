e58d0f8b0b116b5ec86c0febb438c357960b112d
DESKTOP-40GA2OJ\larys <laryssa_olvgyn@hotmail.com>
2021-03-27 15:20:09 -0300

    Project's configuration and implemented login layout

---
+++ app/src/main/java/com/example/empresas/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.empresas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.empresas.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
